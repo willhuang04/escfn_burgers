6eb7781658aa9bf3bec35002646792ee838e95d9
dfunk23-ucla <dfunk23@math.ucla.edu>
2026-03-16 18:43:34 -0700

    Added code to generate tensors of data
    Based on an exact analysis with method of characteristics.

---
+++ CubicFlux_Shock.py
@@ -0,0 +1,29 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n_instances = 50
+n_t = 100
+n_x = 256
+
+uL = np.random.uniform(0.8, 1.2, size=(n_instances, 1, 1))
+uR = np.random.uniform(-0.2, 0.2, size=(n_instances, 1, 1))
+
+s = 1/3*(uL**2 + uL*uR + uR**2)
+
+xs = np.linspace(-5,5,n_x)
+ts = np.linspace(0,5,n_t)
+
+X, T = np.meshgrid(xs, ts)
+
+X_3d = X[np.newaxis, :, :]
+T_3d = T[np.newaxis, :, :]
+
+def u(x,t,s,uL,uR):
+    return np.where(x <= s * t, uL, uR)
+    
+Z = u(X, T, s, uL, uR)
+Z = np.expand_dims(Z, axis=-1)
+
+print(np.shape(Z))
+
+np.save('valData_CubicFlux_Shock_256.npy', Z)
